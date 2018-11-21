a4511730375296e04b41408d4c709550dbf28f5f
Phuc Vu (Robert) <robertvu483@gmail.com>
2018-11-20 16:25:43 -0800

    Added background and color to a few pages

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $("body").prepend('<div id="nav_bar"><div id="home"><a type="button" title="Home" href="home.html"><img src="https://material.io/tools/icons/static/icons/baseline-home-24px.svg"/></a></div><h1>Eat GO</h1><div id="account"><a href="sign_in.html"><img src="https://material.io/tools/icons/static/icons/baseline-account_circle-24px.svg"/>Login</a></div></div>');
+  $("body").prepend('<div id="nav_bar"><div id="home"><a type="button" title="Home" href="home.html"><img src="https://material.io/tools/icons/static/icons/baseline-home-24px.svg"/></a></div><h1>Eat GO</h1><div id="account"><a href="profile.html"><img src="https://material.io/tools/icons/static/icons/baseline-account_circle-24px.svg"/>Login</a></div></div>');
 });
