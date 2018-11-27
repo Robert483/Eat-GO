e78646dde5d20f98bab70e0ce5743d4ceab66abc
Phuc Vu (Robert) <robertvu483@gmail.com>
2018-11-27 15:13:54 -0800

    Fixed shadow and border radius. Added email sign-in method

---
+++ js/firebase_init.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBlaM8Djc6e8gksv4IG3YEben8lyy8BS9k",
+  authDomain: "eat-go-e4a2b.firebaseapp.com",
+  databaseURL: "https://eat-go-e4a2b.firebaseio.com",
+  projectId: "eat-go-e4a2b",
+  storageBucket: "eat-go-e4a2b.appspot.com",
+  messagingSenderId: "368843019209"
+};
+firebase.initializeApp(config);
