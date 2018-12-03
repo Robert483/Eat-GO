967abfaba77f06ca0590cb10b2abc71741c10bd8
Phuc Vu (Robert) <robertvu483@gmail.com>
2018-12-02 23:30:03 -0800

    Add basic map control.

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -1,4 +1,4 @@
-var config = {
+let config = {
   apiKey: "AIzaSyBlaM8Djc6e8gksv4IG3YEben8lyy8BS9k",
   authDomain: "eat-go-e4a2b.firebaseapp.com",
   databaseURL: "https://eat-go-e4a2b.firebaseio.com",
